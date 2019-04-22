ab967ea1b06b1e81652fc9abb1409c1b43b4bd7a
Jonathan Mooney <moonejon@gmail.com>
2019-04-21 21:45:57 -0500

    rendering pokemon cards on screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import MemoryCard from "./components/MemoryCard";
+import Wrapper from "./components/Wrapper";
+import pokemon from "./pokemon.json"
 import './App.css';
 
 class App extends Component {
+    state = {
+        pokemon
+    };
+
   render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+        return (
+            <Wrapper>
+                {this.state.pokemon.map(pokemon => (
+                    <MemoryCard
+                        // removePokemon={this.removePokemon}
+                        id={pokemon.id}
+                        key={pokemon.id}
+                        name={pokemon.name}
+                        image={pokemon.image}
+                    />
+                ))}
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
