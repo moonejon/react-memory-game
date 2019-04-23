8d65f219b7495056947869225057b2a15148d21c
Jonathan Mooney <moonejon@gmail.com>
2019-04-22 22:41:21 -0500

    Updates

---
+++ precache-manifest.1a093e332073d7fe0ecc83c5c05b11ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  },
+  {
+    "revision": "a5436242e675f4851477",
+    "url": "/react-memory-game/static/js/main.0babce72.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a5436242e675f4851477",
+    "url": "/react-memory-game/static/css/main.10258eee.chunk.css"
+  },
+  {
+    "revision": "a7f14282e34d1992f72d226fb6dff652",
+    "url": "/react-memory-game/index.html"
+  }
+];
