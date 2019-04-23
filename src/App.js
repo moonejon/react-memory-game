84fff77d4dfa5a3f91bf33674c42ac8d3b2ac3ca
Jonathan Mooney <moonejon@gmail.com>
2019-04-22 21:21:37 -0500

    added shuffle logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import MemoryCard from "./components/MemoryCard";
+import MemoryCardFront from "./components/MemoryCardFront";
 import Wrapper from "./components/Wrapper";
+import Title from "./components/Title"
 import pokemon from "./pokemon.json"
 import './App.css';
 
+ const shuffleArray = array => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
 class App extends Component {
     state = {
-        pokemon
+        pokemon,
+        // selectedPokemon
     };
 
-  render() {
+    render() {
+      const shuffledCards = shuffleArray(this.state.pokemon)
         return (
             <Wrapper>
-                {this.state.pokemon.map(pokemon => (
-                    <MemoryCard
-                        // removePokemon={this.removePokemon}
+                <Title>
+                    {
+                        <p>
+                            <img src="https://vignette.wikia.nocookie.net/pokemon/images/9/9f/Pok%C3%A9monLogo%28Eng%29.png/revision/latest?cb=20090304221051" />
+                        </p>
+                    }
+                </Title>
+                {shuffledCards.map(pokemon => (
+                    <MemoryCardFront
+                        selectPokemon={this.selectPokemon}
                         id={pokemon.id}
+                        h
                         key={pokemon.id}
                         name={pokemon.name}
                         image={pokemon.image}
